1ac4d7165fff14ad5430f722b292c4aa9cda5dac
alunfes <ts4792@gmail.com>
2016-10-08 13:00:52 +0900

    initial commit

--- HtmlAgilityPackUser/Program.cs
+++ HtmlAgilityPackUser/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Text;
 using System.Linq;
+using System.IO;
 using System.Xml;
 using HtmlAgilityPack;
 
@@ -13,36 +14,50 @@ namespace HtmlAgilityPackUser
 		{
 			//get and check url
 			Console.WriteLine("This is sample program to use some functions of HtmlAgilityPack.");
-			string url=  "0000000000";
+			string url = "0000000000";
 			do
 			{
 				Console.WriteLine("Please input correct url");
 				url = Console.ReadLine();
-			} while(checkUrl(url)==false);
+			} while (checkUrl(url) == false);
 
 			//download html
 			string html = downloadHtml(url);
 
 			//agilitypack
-			if (html.Substring(0,5) != "error")
+			if (html.Substring(0, 5) != "error")
 			{
 				var htmldoc = new HtmlAgilityPack.HtmlDocument();
 				htmldoc.LoadHtml(html);
 
-				var selected = htmldoc.DocumentNode.SelectNodes("//div");
-				foreach (var a in selected)
+				var selected = htmldoc.DocumentNode.SelectNodes("//*");
+
+				try
 				{
-					if (a.OuterHtml.Contains("script")==false)
-						Console.WriteLine(a.XPath.Count(c => c == '/') + " : " + a.InnerText);
+					using (StreamWriter sw = new StreamWriter("data.txt", true, Encoding.Default))
+					{
+						int i = 0;
+						foreach (var a in selected)
+						{
+							if (a.OuterHtml.Contains("script") == false && a.InnerText.Length > 1)
+							{
+								Console.WriteLine(a.XPath.Count(c => c == '/') + " : " + a.InnerText);
+								sw.WriteLine(i.ToString() +"\t"+ a.XPath+"\t"+a.InnerText);
+								i++;
+							}
+						}
+					}
 				}
-				/*var articles = htmldoc.DocumentNode.SelectNodes("*").Select(a => new { title = a.InnerText });
-				foreach (var a in articles)
-					Console.WriteLine(a.title);*/
+				catch (Exception e)
+				{
+					Console.WriteLine("Finished with html download error!");
+					Console.WriteLine(e.ToString());
+				}
 			}
 			else
 			{
 				Console.WriteLine("Finished with html download error!");
-				Console.WriteLine(html.Substring(0,5));
+				Console.WriteLine(html.Substring(0, 5));
 			}
 
 		}
@@ -75,7 +90,7 @@ namespace HtmlAgilityPackUser
 			{
 				Console.WriteLine("Unexpected error occurred!");
 				Console.WriteLine(e.ToString());
-				return "error"+e.ToString();
+				return "error" + e.ToString();
 			}
 		}
 	}
